5564bb4aa00d8589692f12ff09b11d3a888ed40b
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-06-04 11:58:45 +0100

    intro

--- Dictionaries.py
+++ Dictionaries.py
@@ -1,7 +1,29 @@
 programming_dictionary = {
     "Bug": "An error in a program that prevents the program from running as expected.", 
     "Function": "A piece of code that you can easily call over and over again.",
-    "Loop": "The action of doing something over and over again"}
+    }
 
-#to  retrive key
+#retrieving items from dictionary
 print(programming_dictionary["Bug"])
+
+#Adding new items to dictionary.
+programming_dictionary["Loop"] = "The action of doing something over and over again"
+#print(programming_dictionary)
+
+#Empty dictionary
+empty_dictionary = {}
+
+#wipe an existing dictionary
+# programming_dictionary = {}
+# print(programming_dictionary)
+
+#Edit an item in a dictionary
+programming_dictionary["Bug"] = "A moth in your computer"
+#print(programming_dictionary)
+
+#Loop through a dictionary
+for key in programming_dictionary: 
+    print(key)  #it just prints out key
+    print(programming_dictionary[key])
+
+
