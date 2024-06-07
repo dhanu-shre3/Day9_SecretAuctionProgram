626913a5d0350b3bd4e3d96c83b5b5bad6eb7d82
Dhanushree Motanalira Somaiah <motanalirasomaiahd@cardiff.ac.uk>
2024-06-07 11:47:19 +0100

    nesting in dict

---
+++ gradingProgram.py
@@ -0,0 +1,29 @@
+#You have access to a database of student_scores in the format of a dictionary. The keys in student_scores are the names of the students and the values are their exam scores.
+
+#Write a program that converts their scores to grades. By the end of your program, you should have a new dictionary called student_grades that should contain student names for keys and their grades for values.
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+# TODO-1: Create an empty dictionary called student_grades.
+
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student in student_scores:
+  score = student_scores[student]
+  if score > 90:
+    student_grades[student]= "Outstanding"
+  elif score >80:
+    student_grades[student]= "Exceeds Expectations"
+  elif score >70:
+    student_grades[student]= "Acceptable"  
+  else:
+    student_grades[student]= "Fail"  
+    
+
+
+print(student_grades)
